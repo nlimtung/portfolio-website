5b6b7217f81efa949af7ccf583857b0d1eaebb2b
Nicole <nicole@Nicoles-MacBook-Air.local>
2022-04-12 18:10:12 -0400

    adding colour:

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
-const colours = ['red', 'blue', 'green']
 
 $('#hello-text').delay(200).animate({'opacity':'1'},800);
 $('#self-description').delay(400).animate({'opacity':'1'},1000);
+
+
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadein').each( function(i){
+
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},500);
+            }
+
+        });
+
+    });
+});
+
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('#fadein').each( function(i){
+
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},500);
+            }
+
+        });
+
+    });
+});
+
+
 
 
